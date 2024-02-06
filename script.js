e1ea56382112a53d4ce52b3b189837b5bc054483
K0baU <63403741+K0baU@users.noreply.github.com>
2024-02-06 17:07:15 +0900

    Create encrypt()

--- script.js
+++ script.js
@@ -1,3 +1,9 @@
 const pass = document.getElementById("pass");
 const plain = document.getElementById("plain");
 const output = document.getElementById("output");
+
+const encrypt=()=>{
+}
+
+pass.onchange=encrypt;
+plain.onchange=encrypt;
