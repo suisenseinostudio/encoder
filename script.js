ceb5bd78a2bcc821b52b7654989a33c025ed081a
K0baU <63403741+K0baU@users.noreply.github.com>
2024-02-06 17:40:13 +0900

    Define encrypt()

--- script.js
+++ script.js
@@ -2,7 +2,18 @@ const pass = document.getElementById("pass");
 const plain = document.getElementById("plain");
 const output = document.getElementById("output");
 
+const deriveKey=()=>{
+}
+
 const encrypt=()=>{
+  const iv=window.crypto.getRandomValues(new Uint8Array(12));
+  const algo={name:"AES-GCM",iv};
+  const key=deriveKey();
+  const file=plain.files[0];
+  const data=file.arrayBuffer();
+  window.crypto.subtle.encrypt(algo,key,data).then(ab=>{
+    output.href=window.URL.createObjectURL(new File([iv,ab],file.name+"-e"));
+  });
 }
 
 pass.onchange=encrypt;
