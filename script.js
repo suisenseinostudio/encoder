601a593ebdb8806bf8a620c222660d60ed5ae0e8
K0baU <63403741+K0baU@users.noreply.github.com>
2024-02-10 11:31:59 +0900

    Update script.js

--- script.js
+++ script.js
@@ -7,14 +7,13 @@ const output = document.getElementById("output");
 const encrypt=async()=>{
   if(pass.value==""||plain.files.length==0)return;
   const iv=window.crypto.getRandomValues(new Uint8Array(12));
-  console.log(`iv:${iv}`);
   const algo={name:"AES-GCM",iv};
   const key=await deriveKey(pass.value);
   const file=plain.files[0];
   const data=await (new Blob([iv,file])).arrayBuffer();
-  console.log(`data:${new Uint8Array(data)}`);
+  console.log(`enc(${algo},key(${pass.value}),${data})`);
   const ab=await window.crypto.subtle.encrypt(algo,key,data);
-  console.log(new Uint8Array(ab));
+  console.log(`result:${new Uint8Array(ab)}`);
   const dlFile=new File([iv,ab],file.name+"-e");
   output.href=window.URL.createObjectURL(dlFile);
   output.textContent="download encrypted file";
