46c147a3a001dfadf32b5dfb8148f2fd3dfc4bc1
K0baU <63403741+K0baU@users.noreply.github.com>
2024-02-10 11:26:35 +0900

    Update script.js

--- script.js
+++ script.js
@@ -1,25 +1,15 @@
+impore {deriveKey} from "https://raw.githubusercontent.com/suisenseinostudio/deriveKey/main/derive-key.js";
+
 const pass = document.getElementById("pass");
 const plain = document.getElementById("plain");
 const output = document.getElementById("output");
-
-const importKey=async()=>{
-  const data=(new TextEncoder()).encode(pass.value);
-  return window.crypto.subtle.importKey("raw",data,"PBKDF2",false,["deriveKey"]);
-};
-
-const deriveKey=async()=>{
-  const salt=window.crypto.getRandomValues(new Uint8Array(16));
-  const algo={name:"PBKDF2",hash:"SHA-256",salt,iterations:100000};
-  const base=await importKey();
-  return window.crypto.subtle.deriveKey(algo,base,{name:"AES-GCM",length:256},false,["encrypt"]);
-};
 
 const encrypt=async()=>{
   if(pass.value==""||plain.files.length==0)return;
   const iv=window.crypto.getRandomValues(new Uint8Array(12));
   console.log(`iv:${iv}`);
   const algo={name:"AES-GCM",iv};
-  const key=await deriveKey();
+  const key=await deriveKey(pass.value);
   const file=plain.files[0];
   const data=await (new Blob([iv,file])).arrayBuffer();
   console.log(`data:${new Uint8Array(data)}`);
